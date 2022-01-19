047ef72218e265b55d2ac8a295c717eccb608ee9
maxcurie1996 <maxcurie@utexas.edu>
2022-01-18 19:49:24 -0800

    Working calculation

--- SLiM_NN/0MTMDispersion_list_Calc_parallel.py
+++ SLiM_NN/0MTMDispersion_list_Calc_parallel.py
@@ -16,67 +16,52 @@ sys.path.insert(1, './../Tools')
 from DispersionRelationDeterminantFullConductivityZeff import Dispersion
 
 #**********Start of user block***************
-path='.'
-Input_csv=path+'/parameter_list.csv'   
+path='.'   
 Output_csv=path+'/0MTM_scan.csv'
 
-nu_list=np.arange(0,10,1.)
+nu_list=np.arange(1.,10,1.)
 zeff_list=np.arange(1,2.5,0.5)
 eta_list=np.arange(0.5,5,0.8)
-shat_list=np.arange(0,0.1,0.02)
-beta_list=np.arange(0.0001,0.003,0.0005)
+shat_list=np.arange(0.02,0.1,0.02)
+beta_list=np.arange(0.0005,0.003,0.0005)
 ky_list=np.arange(0.01,0.1,0.01)
 mu_list=np.arange(0,2,0.3)
 xstar=10.
 ModIndex=1 #global dispersion
 #**********end of user block****************
-df=pd.read_csv(Input_csv)
 
 def Dispersion_calc(para_list):
-    [nu,zeff,eta,shat,beta,ky,ModIndex,mu,xstar,Output_csv,Input_csv,i]=para_list
+    [nu,zeff,eta,shat,beta,ky,ModIndex,mu,xstar,Output_csv]=para_list
     
     w0=Dispersion(nu,zeff,eta,shat,beta,ky,ModIndex,mu,xstar) 
 
     omega=np.real(w0)
-    omega_kHz=omega*df['omega_n_kHz'][i]
     gamma=np.imag(w0)
-    gamma_cs_a=gamma*df['omega_n_cs_a'][i]
     
     with open(Output_csv, 'a+', newline='') as csvfile: #adding a row
         csv_data = csv.writer(csvfile, delimiter=',')
-        csv_data.writerow([ df['n'][i],df['m'][i],df['rho_tor'][i],\
-                    omega_kHz,omega_kHz+df['omega_e_lab_kHz'][i]-df['omega_e_plasma_kHz'][i],gamma_cs_a,\
-                    df['omega_n_kHz'][i],df['omega_n_cs_a'][i],\
-                    df['omega_e_plasma_kHz'][i],df['omega_e_lab_kHz'][i],
-                    df['peak_percentage'][i],df['nu'][i],\
-                    df['zeff'][i],df['eta'][i],\
-                    df['shat'][i],df['beta'][i],df['ky'][i],\
-                    df['ModIndex'][i],df['mu'][i],df['xstar'][i] ])
+        csv_data.writerow([ omega, gamma,nu,zeff,eta,shat,beta,ky,\
+                ModIndex,mu,xstar ])
     csvfile.close()
     return w0
 
 if __name__ == '__main__':      
     with open(Output_csv, 'w', newline='') as csvfile:     #clear all and then write a row
         csv_data = csv.writer(csvfile, delimiter=',')
-        csv_data.writerow(['n','m','rho_tor',\
-                'omega_plasma_kHz','omega_lab_kHz',\
-                'gamma_cs_a','omega_n_kHz',\
-                'omega_n_cs_a','omega_e_plasma_kHz',\
-                'omega_e_lab_kHz','peak_percentage',\
+        csv_data.writerow(['omega_omega_n','gamma_omega_n',\
                 'nu','zeff','eta','shat','beta','ky',\
                 'ModIndex','mu','xstar'])
     csvfile.close()
 
     para_list=[]
-    for nu in tqdm(nu_list):
+    for nu in nu_list:
         for zeff in zeff_list:
             for eta in eta_list:
                 for shat in shat_list:
                     for beta in beta_list:
                         for ky in ky_list:
                             for mu in mu_list:
-                                para_list.append([nu,zeff,eta,shat,beta,ky,ModIndex,mu,xstar,Output_csv,Input_csv,i])
-    
+                                para_list.append([nu,zeff,eta,shat,beta,ky,ModIndex,mu,xstar,Output_csv])
     
     print('***********************************')
     print('*********paraellel calcuation******')
