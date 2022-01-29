7897ad4b9f87795de936e2d9776d785c135557a5
maxcurie1996 <maxcurie@utexas.edu>
2022-01-28 18:31:21 -0800

    Working MPI code
    Dispersion code now runs in supercomputers!

--- SLiM_NN/Dispersion_NN.py
+++ SLiM_NN/Dispersion_NN.py
@@ -11,13 +11,31 @@ import time
 
 #****************************************
 #**********start of user block***********
-
-filename='./0MTM_scan_2022_01_19.csv'
+filename='./NN_data/0MTM_scan.csv'
 epochs = 10
 batch_size = 100
-testing_output=False
+testing_output=True
 #**********end of user block*************
 #****************************************
+
+#*********start of creating of model***************
+def create_model():
+    
+    #creating the model
+    model = keras.Sequential([
+        keras.layers.Dense(units=6, input_shape=(1,), activation='relu'),                      # input layer (1)
+        keras.layers.Dense(units=64, activation='relu'), # hidden layer (2)
+        keras.layers.Dense(units=64, activation='relu'), # hidden layer (3)
+        keras.layers.Dense(units=2, activation='softmax') # output layer (4)
+    ])
+    print(model.summary())
+    
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+                  loss=tf.keras.losses.BinaryCrossentropy(),
+                  metrics=[tf.keras.metrics.BinaryAccuracy(),
+                           tf.keras.metrics.FalseNegatives()])
+    #*********end of creating of model***************
+    return model
 
 #*******start of loading data*******************
 df=pd.read_csv(filename)
@@ -33,45 +51,26 @@ df=df.query('omega_omega_n!=0')
 df_x=pd.DataFrame(np.transpose([df['nu'],df['zeff'],\
                     df['eta'],df['shat'],\
                     df['ky'],df['mu']/df['xstar']]),
-                  #index=list('abc'),
                   columns=['nu', 'zeff','eta','shat',\
                     'ky','mu_norm'])
 df_y=pd.DataFrame(np.transpose([df['omega_omega_n'],\
                     df['gamma_omega_n']]),
-                  #index=list('abc'),
                   columns=['omega_norm','gamma_norm'])
 
 x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2)
 
 if testing_output==True:
-    print(df_x)
-    print(df_y)
-    print(len(df_x))
-    print(len(df_y))
+    #print(df_x)
+    #print(df_y)
+    #print(len(df_x))
+    #print(len(df_y))
+    print('x_train')
     print(x_train)
+    print('y_train')
+    print(y_train)
 
-#x=[df['nu'],df['zeff'],df['eta'],df['shat'],df['ky'],df['mu']/df['xstar']]
-#y=[df['omega_plasma_kHz']/df['omega_n_kHz'],df['gamma_cs_a']/df['omega_n_cs_a']]
-
-#x_train=x[:int(len(df)*0.8)]
-#y_train=y[:int(len(df)*0.8)]
-
-#x_test=x[int(len(df)*0.8):]
-#y_test=y[int(len(df)*0.8):]
 #*******end of  of loading data*******************
 
-
-#*********start of creating of model***************
-
-#creating the model
-'''
-model = keras.Sequential([
-    keras.layers.Dense(input_shape=(6)),                      # input layer (1)
-    keras.layers.Dense(64, activation='relu'), # hidden layer (2)
-    keras.layers.Dense(64, activation='relu'), # hidden layer (3)
-    keras.layers.Dense(2, activation='softmax') # output layer (4)
-])
-'''
 gpus=tf.config.list_physical_devices('GPU')
 if gpus:
     try:
@@ -85,20 +84,10 @@ if gpus:
         print(e)
 
 
-model = keras.Sequential([])
-model.add(tf.keras.Input(shape=(6,)))
-model.add(tf.keras.layers.Dense(64))
-model.add(tf.keras.layers.Dense(64))
-model.add(tf.keras.layers.Dense(2))
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-print(len(model.weights))
-#*********end of creating of model***************
-
 #*********start of trainning***********************
 start = time.time()
 #with tf.device('/GPU:0'):
+model=create_model()
 model.fit(x_train, y_train, epochs=epochs)  # we pass the data, labels and epochs and watch the magic!
 end = time.time()
 print(f"Runtime of the program is {end - start} s")
